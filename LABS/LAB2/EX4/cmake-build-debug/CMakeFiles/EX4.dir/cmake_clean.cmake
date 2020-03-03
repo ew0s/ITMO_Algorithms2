de627d10cfb0eefbd3e0a2cb4453d4227de7a260
ewos365@mail.ru <Fomesare1>
2020-03-03 22:11:57 +0300

    NEW

---
+++ LABS/LAB2/EX4/cmake-build-debug/CMakeFiles/EX4.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EX4.dir/main.cpp.o"
+  "EX4"
+  "EX4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EX4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
