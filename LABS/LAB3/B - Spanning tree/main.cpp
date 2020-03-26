99d51d21df998412b91c2eef774c47d2e48ffc4e
Egor Savkovskiy <ewos365@mail.ru>
2020-03-26 18:55:20 +0300

    new solutions

--- LABS/LAB3/B - Spanning tree/main.cpp
+++ LABS/LAB3/B - Spanning tree/main.cpp
@@ -1,85 +1,58 @@
+#include <iostream>
 #include <vector>
 #include <cmath>
-#include <iostream>
-#include <set>
-#include <fstream>
+constexpr int MAX = 31415926535;
+using std::cin;
+using std::cout;
 
-#define MAX 10000
-
-
-void findMST (std::vector<std::vector <double>> &graph,
-              std::vector <double> &min_weight,
+void findMST (std::vector <std::vector <int>> graph,
+              std::vector <int> &min_weight,
               std::vector <bool> &used,
               int n)
 {
-    for (int i = 0; i < n; i++) {
+    min_weight[0] = 0;
+    for (size_t i = 0; i < n; i++) {
         int v = -1;
-        for (int j = 0; j < n; j++) {
+        for (size_t j = 0; j < n; j++)
             if (!used[j] && (v == -1 || min_weight[j] < min_weight[v]))
                 v = j;
-        }
-
         used[v] = true;
-
-        for (int to = 0; to < n; to++) {
-            if (graph[v][to] < min_weight[to]) {
-                min_weight[to] = graph[v][to];
-            }
-        }
+        for (size_t j = 0; j < n; j++)
+            if (!used[j] && graph[v][j] < min_weight[j] && v != j)
+                min_weight[j] = graph[v][j];
     }
 }
 
-
-struct top {
-    int x_;
-    int y_;
-};
-
-
 int main() {
-    std::ifstream fin ("spantree.in");
-    std::ofstream fOut ("spantree.out");
-    std::ios_base::sync_with_stdio(false); std::cin.tie(NULL);
+    std::ios_base::sync_with_stdio(false);
 
     int n = 0;
-    fin >> n;
+    cin >> n;
 
-    std::vector <top> graph_field;
-    std::vector <std::vector <double>> graph (n, std::vector<double> (n, MAX));
-    std::vector <double> min_weight (n, MAX);
-    std::vector <bool> used (n);
+    std::vector <std::pair <int, int>> graph_field (n);
+    std::vector <std::vector <int>> graph (n, std::vector<int> (n));
+    std::vector <int> min_weight (n, MAX);
+    std::vector <bool> used (n, false);
 
-    int x = 0, y = 0;
-
-    for (size_t i = 0; i < n; i++) {
-
-        fin >> x >> y;
-        top newTop = {x, y};
-        graph_field.push_back(newTop);
-    }
-    fin.close();
+    for (auto& field : graph_field)
+        cin >> field.first >> field.second;
 
     for (size_t i = 0; i < n; i++) {
-        for (size_t j = i + 1; j < n; j++) {
+        for (size_t j = 0; j < n; j++) {
 
-            double distance = sqrt(pow(graph_field[j].x_ - graph_field[i].x_, 2) +
-                                   pow(graph_field[j].y_ - graph_field[i].y_, 2));
-
-            graph[i][j] = distance;
-            graph[j][i] = distance;
+            graph[i][j] = pow(graph_field[i].first - graph_field[j].first, 2) + pow(graph_field[i].second - graph_field[j].second, 2);
         }
     }
 
-    min_weight[0] = 0;
+
     findMST(graph, min_weight, used, n);
 
     double weightMST = 0;
-    for (double i : min_weight) {
-        weightMST += i;
+    for (size_t i = 0; i < n; i++) {
+        weightMST += sqrt(min_weight[i]);
     }
-    fOut << weightMST;
-    fOut.close();
-
+    std::cout.precision(10);
+    cout << weightMST << "\n";
 
     return 0;
 }
